fb6520cda88d3451af8a7bb2d060c8c1928956f8
Guilherme Gomes <guilhermeneto26@hotmail.com>
2025-07-30 21:18:17 -0300

    feat: Implements user controller

--- backend/app.js
+++ backend/app.js
@@ -4,6 +4,7 @@ import express from 'express';
 import {PORT} from './config/env.js';
 import connectToDatabase from './database/mongodb.js';
 
+import userRouter from './routes/user.routes.js';
 import authRouter from './routes/auth.routes.js';
 import authorize from './middlewares/auth.middleware.js';
 const app = express();
@@ -13,6 +14,8 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
 app.use('/api/SmartVet/auth',authRouter);
+
+app.use('/api/SmartVet/users', userRouter);
 app.get('/', (req, res) => {
   res.send('Welcome to the SmartVet API');
 });
